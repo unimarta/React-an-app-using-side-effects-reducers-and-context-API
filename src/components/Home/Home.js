d5f9a075c00a3ab73b376d25fe8cc9a7e0873df2
unimarta <kramkagitarzystka@yahoo.pl>
2022-02-21 11:00:03 +0100

    Added dynamic context

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,9 +8,7 @@ const Home = (props) => {
   return (
     <Card className={classes.home}>
       <h1>Welcome back!</h1>
-      <Button className={classes.btn} onClick={props.onLogout}>
-        Logout
-      </Button>
+      <Button onClick={props.onLogout}>Logout</Button>
     </Card>
   );
 };
